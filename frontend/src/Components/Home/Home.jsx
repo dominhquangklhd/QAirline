035028221e4ceb75d3c4eb0fd9184f74ea24bba0
2uangthor <minhquangkl04@gmail.com>
2024-11-18 23:05:13 +0700

    the first home page

---
+++ frontend/src/Components/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+const Home = () => {
+  useEffect(() => {
+    Aos.init({duration: 2000})
+  }, [])
+
+  return (
+    <div className='home flex container'>
+      <div className='mainText'>
+        <h1 data-aos='fade-up' data-aos-duration='2500'>Create Ever-lasting Memonies With Us</h1>
+      </div>
+
+      <div className='homeImages flex'>
+        <div className="videoDiv">
+          <video src="/assets/sky.mp4" autoPlay muted loop className='video'>
+
+          </video>
+        </div>
+
+        <img src="assets/plane-nobg.png" alt="" className='plane'/>
+      </div>
+    </div>
+  )
+}
+
+export default Home
