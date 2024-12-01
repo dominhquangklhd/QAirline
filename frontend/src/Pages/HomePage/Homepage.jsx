9c35f2f6c562dae933218bf382350c1201077bff
PhatNguyenDuc <phat20041304@gmail.com>
2024-12-01 16:17:33 +0700

    ..

--- frontend/src/Pages/HomePage/Homepage.jsx
+++ frontend/src/Pages/HomePage/Homepage.jsx
@@ -5,21 +5,23 @@ import FlightSearch from "../SearchComponent/FlightSearch";
 import Subscribers from "./Subscribers";
 import Support from "./Support";
 import Travelers from "./Travelers";
-import Navbar from "./Navbar"
+import Navbar from "./Navbar";
 
 function HomePage() {
   return (
     <div>
-        <Navbar/>    
-        <Home></Home>
-        <Search></Search>
-        <Support></Support>
-        <Info></Info>
-        <Lounge></Lounge>
-        <Travelers></Travelers>
-        <Subscribers></Subscribers>
+      <Navbar />
+      <Home></Home>
+      {/* <Search></Search>
+       */}
+      <FlightSearch></FlightSearch>
+      <Support></Support>
+      <Info></Info>
+      <Lounge></Lounge>
+      <Travelers></Travelers>
+      <Subscribers></Subscribers>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
