824dd7be10d12029230fbbc05e89ea3e717ec832
2uangthor <minhquangkl04@gmail.com>
2024-12-01 15:08:06 +0700

    add HomePage again

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -1,25 +1,34 @@
+import React, { useRef } from 'react';
+import Navbar from './Navbar';
 import Home from './Home';
+import FlightSearch from '../SearchComponent/FlightSearch';
+import Support from './Support';
 import Info from './Info';
 import Lounge from './Lounge';
-import Search from './Search';
-import Subscribers from "./Subscribers";
-import Support from "./Support";
-import Travelers from "./Travelers";
-import Navbar from "./Navbar"
+import Travelers from './Travelers';
+import Subscribers from './Subscribers';
 
 function HomePage() {
+  const flightSearchRef = useRef(null);
+
+  const handleScrollToFlightSearch = () => {
+    if (flightSearchRef.current) {
+      flightSearchRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div>
-        <Navbar/>    
-        <Home></Home>
-        <Search></Search>
-        <Support></Support>
-        <Info></Info>
-        <Lounge></Lounge>
-        <Travelers></Travelers>
-        <Subscribers></Subscribers>
+      <Navbar onSearchClick={handleScrollToFlightSearch} />
+      <Home />
+      <FlightSearch ref={flightSearchRef} />
+      <Support />
+      <Info />
+      <Lounge />
+      <Travelers />
+      <Subscribers />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
