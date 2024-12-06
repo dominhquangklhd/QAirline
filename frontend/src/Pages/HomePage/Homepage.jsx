6f46192c8eb89f07c284602f199674cd16f02d3c
2uangthor <minhquangkl04@gmail.com>
2024-12-06 22:51:15 +0700

    fix homePage

---
+++ frontend/src/Pages/HomePage/Homepage.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+import Home from './Home';
+import FlightSearch from '../SearchComponent/FlightSearch';
+import Support from './Support';
+import Info from './Info';
+import Lounge from './Lounge';
+import Travelers from './Travelers';
+import Subscribers from './Subscribers';
+
+import { smoothScrollTo } from "../../CommonFunctions/SmoothScroll";
+
+function HomePage({ flightSearchRef }) {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.scrollToFlightSearch && flightSearchRef.current) {
+      smoothScrollTo(flightSearchRef);
+    }
+  }, [location.state, flightSearchRef]);
+
+  return (
+    <div>
+      <Home />
+      <FlightSearch ref={flightSearchRef} />
+      <Support />
+      <Info />
+      <Lounge />
+      <Travelers />
+      <Subscribers />
+    </div>
+  );
+}
+
+HomePage.propTypes = {
+  flightSearchRef: PropTypes.shape({
+    current: PropTypes.any, // `current` thường là DOM element hoặc null
+  }),
+};
+
+export default HomePage;
