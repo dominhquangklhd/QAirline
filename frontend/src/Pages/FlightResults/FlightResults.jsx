3cbb553012eae1efd61cdd9d96c510b319d62481
2uangthor <minhquangkl04@gmail.com>
2024-12-18 12:52:21 +0700

    fix, add sth in BookingUserInfo

--- frontend/src/Pages/FlightResults/FlightResults.jsx
+++ frontend/src/Pages/FlightResults/FlightResults.jsx
@@ -93,7 +93,7 @@ const FlightResults = () => {
                   <FlightCardV2
                     flight={flight}
                     onSelect={(selectedFlight) =>
-                      handleFlightSelection(selectedFlight, "outbound")
+                      handleFlightSelection(selectedFlight, "")
                     }
                     isSelected={selectedOutbound?.id === flight.id}
                   />
