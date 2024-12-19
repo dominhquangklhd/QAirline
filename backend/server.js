497ae7d1fc392edcd1760254bb3e6ea4904f989b
PhatNguyenDuc <phat20041304@gmail.com>
2024-12-19 14:15:08 +0700

    complete Cancelation for guest

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,12 @@ const app = express();
 app.use(
   cors({
     origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 const authRoutes = require("./src/routes/authRoutes");
 const flightRoutes = require("./src/routes/flightRoutes");
